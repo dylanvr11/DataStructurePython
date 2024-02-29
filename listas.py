674c974b3b95622e0c3eaeab0970155cf7aa485c
dylanvr11 <dylanvr97@gmail.com>
2024-02-28 20:52:18 -0500

    sublists

---
+++ listas.py
@@ -0,0 +1,12 @@
+lista = ["a", "b", "c"]
+lista_distinta = ["A", 2, "C", 4.4]
+#print(lista_distinta[0])
+lista[1] = "8"
+#print(lista)
+
+#--------------------------------------------------
+lista_grande = ["Python","Java","C++","JavaScript"]
+lista_peque = lista_grande[0:3]
+#print(lista_peque)
+lista_grande[3:5] = ["C#","PHP"]
+print(lista_grande)
