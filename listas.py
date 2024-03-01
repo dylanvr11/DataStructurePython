950dee0217fb2d2075be297149c03b080b126024
dylanvr11 <dylanvr97@gmail.com>
2024-02-29 21:24:10 -0500

    Functions of the lists

--- listas.py
+++ listas.py
@@ -1,12 +1,36 @@
-lista = ["a", "b", "c"]
-lista_distinta = ["A", 2, "C", 4.4]
-#print(lista_distinta[0])
-lista[1] = "8"
-#print(lista)
+list = ["a", "b", "c"]
+different_list = ["A", 2, "C", 4.4]
+#print(different_list[0])
+list[1] = "8"
+#print(list)
 
 #--------------------------------------------------
-lista_grande = ["Python","Java","C++","JavaScript"]
-lista_peque = lista_grande[0:3]
-#print(lista_peque)
-lista_grande[3:5] = ["C#","PHP"]
-print(lista_grande)
+large_list = ["Python","Java","C++","JavaScript"]
+small_list = large_list[0:3]
+#print(small_list)
+large_list[3:5] = ["C#","PHP"]
+#print(large_list)
+#-------------------------------------------------
+list_1 = [1,3]
+list_1.append(4) #Adding to the list item at the end
+#print(list_1)
+list_1.append([5,6])
+#print(list_1) # [1, 3, 4, [5, 6]]
+list_1.insert(1,2) #(index,value)
+#print(list_1)
+
+letters = ["x","y","z"]
+letters.remove("z") #Removing the value
+#print(letters)
+
+number = [1,2,2,2,2,3,4,5]
+number.remove(2) #Removes the first element with value 2 
+print(number.count(2)) #Count how many elements with that value there
+#print(number)
+
+names = ["Juan","Miguel","Silvia"]
+names.pop() #Removes the last item from the list
+#print(names)
+
+names.pop(1) #Passing the index parameter
+print(names)
